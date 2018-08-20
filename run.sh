1b7d2dfef81698a4fc8e02bbf54b4a5098d0d826
Tom Troyer <tom.troyer@gmail.com>
2018-08-19 23:54:08 -0400

    Adding program depenency check

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Check script dependencies
+command -v wget >/dev/null 2>&1 || { echo >&2 "Cannot locate wget. Is it installed? Aborting."; exit 1; }
+command -v gunzip >/dev/null 2>&1 || { echo >&2 "Cannot locate gunzip. Is it installed? Aborting."; exit 1; }
+command -v sqlite3 >/dev/null 2>&1 || { echo >&2 "Cannot locate sqlite3. Is it installed? Aborting."; exit 1; }
 
 # Make sure pwd is script path
 cd "$(dirname "$0")"
