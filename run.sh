0ee52137c3c3a0548c3076be3478c65a0e918a88
Tom Troyer <tom.troyer@gmail.com>
2022-06-12 20:29:55 -0400

    Fixing issue with unescaped quotes

--- run.sh
+++ run.sh
@@ -9,20 +9,37 @@ cd "$(dirname "$0")"
 
 # Create data and download files into it
 mkdir ./data
+echo "----------------------------------"
+echo "Downloading data."
+sleep 3
 wget -r -np -l 1 -A gz -nH --cut-dirs=1 --directory-prefix=data https://datasets.imdbws.com/
 
 # Unpack data
+echo "----------------------------------"
 echo "Download complete.  Unzipping data."
+sleep 3
 cd data
 gunzip *.gz
 cd ..
 
+# Convert quotes
+echo "----------------------------------"
+echo "Escaping quotes for sqlite import."
+sleep 3
+cd data
+sed -i 's/\"/\\\"/g' *.tsv
+cd ..
+
 # Create database and import
 rm imdb.db
+echo "----------------------------------"
 echo "Creating database."
+sleep 3
 sqlite3 imdb.db < import.sql
 
 # Cleanup
 rm ./data/*.tsv
 
+echo "----------------------------------"
 echo "Process completed.  imdb.db created."
+echo "----------------------------------"
